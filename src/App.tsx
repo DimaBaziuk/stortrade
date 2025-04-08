bc9ba9f27965c2fd31cdaea03a6f429bb60dea88
Dmytro <mitrobaz@gmail.com>
2025-04-08 23:59:32 +0300

    start working on the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.scss";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Routers from "./router/Routes";
 
 const App: React.FC = () => {
-    return <></>;
+    return (
+        <>
+            <Header />
+            <Routers />
+            <Footer />
+        </>
+    );
 };
 
 export default App;
