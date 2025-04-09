d4ff6535002cd2fbf5e2d3d06ad74e80ad435923
Dmytro <mitrobaz@gmail.com>
2025-04-09 15:19:59 +0300

    create the basic pages and routes

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home/Home";
+import Help from "../pages/Help/Help";
+import Account from "../pages/Account/Account";
+import Catalog from "../pages/Catalog/Catalog";
+import Product from "../pages/Product/Product";
 
 const Routers = () => {
     return (
         <Routes>
             <Route index element={<Home />} />
+            <Route path="/account" element={<Account />} />
+            <Route path="/help" element={<Help />} />
+            <Route path="/catalog" element={<Catalog />} />
+            <Route path="/product/:id" element={<Product />} />
         </Routes>
     );
 };
